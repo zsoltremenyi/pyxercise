01dae022b90621dde7fdc3e8d7ca01296a366169
Zsolt Remenyi <remenyi1zsolt@gmail.com>
2023-04-23 17:52:04 +0100

    determining compound interest basic version.

--- det_comp_int.py
+++ det_comp_int.py
@@ -1,17 +1,18 @@
 import math
 
+
+def compound_interest(amount, rate, years, comperyear):
+    rate = rate / 100
+    end_of_investment = round(amount * math.pow((1 + rate / comperyear), comperyear * years), 2)
+    return f"${amount} invested at {rate * 100}% for {years} years compounded {compound}" \
+           f" times per year is ${end_of_investment}."
+
 try:
     principal_amount = float(input("What is the principal amount? "))
     an_rate = float(input("What is the rate? "))
     num_years = int(input("What is the number of years? "))
     compound = int(input("What is the number of times the interest is compounded per year? "))
 
-
-    def compound_interest(amount, rate, years, comperyear):
-        rate = rate / 100
-        end_of_investment = round(amount * math.pow((1 + rate / comperyear), comperyear * years), 2)
-        return f"${amount} invested at {rate * 100}% for {years} years compounded {compound}" \
-               f" times per year is ${end_of_investment}."
 
 except ValueError:
     print("Please enter valid value")
