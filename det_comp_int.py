644ac19b1ddc5a4f081e8029f0a8e86819f8cbdf
Zsolt Remenyi <remenyi1zsolt@gmail.com>
2023-04-24 18:04:23 +0100

    starting GUI

--- det_comp_int.py
+++ det_comp_int.py
@@ -1,21 +1,29 @@
 import math
+from tkinter import *
+from tkinter import ttk
 
+root = Tk()
+frame = ttk.Frame(root, padding=10)
+frame.grid()
+ttk.Label(frame, text="Hello World!").grid(column=0, row=0)
+ttk.Button(frame, text="Quit", command=root.destroy).grid(column=1, row=0)
+root.mainloop()
 
-def compound_interest(amount, rate, years, comperyear):
-    rate = rate / 100
-    end_of_investment = round(amount * math.pow((1 + rate / comperyear), comperyear * years), 2)
-    return f"${amount} invested at {rate * 100}% for {years} years compounded {compound}" \
-           f" times per year is ${end_of_investment}."
-
-try:
-    principal_amount = float(input("What is the principal amount? "))
-    an_rate = float(input("What is the rate? "))
-    num_years = int(input("What is the number of years? "))
-    compound = int(input("What is the number of times the interest is compounded per year? "))
-
-
-except ValueError:
-    print("Please enter valid value")
-
-else:
-    print(compound_interest(principal_amount,an_rate,num_years,compound))
+# def compound_interest(amount, rate, years, comperyear):
+#     rate = rate / 100
+#     end_of_investment = round(amount * math.pow((1 + rate / comperyear), comperyear * years), 2)
+#     return f"${amount} invested at {rate * 100}% for {years} years compounded {compound}" \
+#            f" times per year is ${end_of_investment}."
+#
+# try:
+#     principal_amount = float(input("What is the principal amount? "))
+#     an_rate = float(input("What is the rate? "))
+#     num_years = int(input("What is the number of years? "))
+#     compound = int(input("What is the number of times the interest is compounded per year? "))
+#
+#
+# except ValueError:
+#     print("Please enter valid value")
+#
+# else:
+#     print(compound_interest(principal_amount,an_rate,num_years,compound))
