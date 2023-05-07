7f4c5bc0bba16a57b92e5b4478bc33f2af8325e9
Zsolt Remenyi <remenyi1zsolt@gmail.com>
2023-05-08 00:52:56 +0100

    basic exercise

--- taxcalc/taxcalc.py
+++ taxcalc/taxcalc.py
@@ -1,1 +1,5 @@
+order_amount = int(input("What is the order amount? "))
+state = input("What is the state? ").lower()
 
+if state == "wi" or state == "wisconsin":
+    print(f'The subtotal is ${order_amount}.\nThe tax is ${order_amount * 0.055}.\nThe total is ${round(order_amount*1.055, 2)}.')
