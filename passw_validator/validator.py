9fa2a457912e0301db02e837b26a0427c539662e
Zsolt Remenyi <remenyi1zsolt@gmail.com>
2023-05-08 14:45:59 +0100

    hashed with bcrypt and compared with input, only works from terminal(?)

--- passw_validator/validator.py
+++ passw_validator/validator.py
@@ -1,8 +1,19 @@
-login = {"dirk": "abc123"}
+import getpass
+import pwinput
+import bcrypt
 
-password = input("What is the password? ")
+print(bcrypt)
+login = {"dirk": b"$2b$12$qWDVJeKX7DSJRNb7USJLR.KhDB9bj22vkTEMSE4jkLRClHkvB1Z5a", #abc123
+         "bubba": b"$2b$12$V1X2mx4fwg411YXMvNtzYevEukJ8SQO.SR5rpucIahLjDtRBLOkIa", #if you find this one out, i'll buy you a beer
+         "jacque": b"$2b$12$EjsosXzfTpLO4ihdIgGGau7aR/RCOgziSoiMc/AI/3FiWeFwnKkG", #CsCSSCsC
+         }
 
-if password == login["dirk"]:
+
+password = pwinput.pwinput(prompt="What is your password? ", mask="*")
+
+
+if bcrypt.checkpw(bytes(password, encoding="ascii"), login["dirk"]):
     print("Welcome!")
 else:
     print("I don't know you.")
+
