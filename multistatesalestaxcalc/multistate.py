5683c4b9708592fbfcc9b1711f101daabc3396a5
Zsolt Remenyi <remenyi1zsolt@gmail.com>
2023-05-24 14:13:11 +0100

    working version

--- multistatesalestaxcalc/multistate.py
+++ multistatesalestaxcalc/multistate.py
@@ -1,6 +1,24 @@
-from bs4 import BeautifulSoup as bs
-import requests
+from statenameapi import State
+from calculator import Calculator
+import states
 
-order_amount = input("What is the order amount? ")
-state = input("What state do you live in? ")
+order_amount = float(input("What is the order amount? "))
+state = input("What state do you live in? (full name or postal abbriviation) ").title()
+
+
+state_finder = State(state.upper())
+
+
+
+if state_finder.full_name():
+    calc = Calculator(order_amount, state_finder.full_name())
+    print(calc.calculator())
+elif state in states.state_and_tax:
+    calc = Calculator(order_amount, state)
+    print(calc.calculator())
+
+
+
+
+# print(state_finder.full_name())
 
