3080bba13e4a9bd36bc7926b0937fb2b236061f4
Zsolt Remenyi <remenyi1zsolt@gmail.com>
2023-05-24 23:26:21 +0100

    ' (ch)' removed from end of state name

--- multistatesalestaxcalc/states.py
+++ multistatesalestaxcalc/states.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup as bs
 import requests
+import re
 
 URL = "https://taxfoundation.org/2021-sales-taxes/"
 
@@ -8,5 +9,7 @@ site =requests.get(URL)
 soup = bs(site.content, features="html.parser")
 table_rows = soup.find_all('tr')
 
+
 states = [n.splitlines() for n in [i.text.strip() for i in table_rows]]
-state_and_tax = {i[0].replace(u"\xa0", u" "):i[1].replace("%", "") for i in states[1:-1]}
+state_and_tax = {re.sub(" \(.\)", "", i[0].replace(u"\xa0", u" ")):i[1].replace("%", "") for i in states[1:-1]}
+print(state_and_tax)
