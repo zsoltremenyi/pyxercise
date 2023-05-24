03365ea4fd97e25c4f42db520cebd9f419cc4bc2
Zsolt Remenyi <remenyi1zsolt@gmail.com>
2023-05-24 14:15:05 +0100

    sales tax calculator

---
+++ multistatesalestaxcalc/calculator.py
@@ -0,0 +1,13 @@
+import states
+
+
+class Calculator:
+    salestax_dict = states.state_and_tax
+
+    def __init__(self, amount, state):
+        self.amount = amount
+        self.state = state
+
+    def calculator(self):
+        tax = float(self.salestax_dict[self.state])
+        return round(self.amount*(1+tax))
