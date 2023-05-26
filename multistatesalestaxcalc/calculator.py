461596801da526e9e3471438d936416fde1d6ddd
Zsolt Remenyi <remenyi1zsolt@gmail.com>
2023-05-26 14:38:56 +0100

    tax_amount function added, module final

--- multistatesalestaxcalc/calculator.py
+++ multistatesalestaxcalc/calculator.py
@@ -7,7 +7,14 @@ class Calculator:
     def __init__(self, amount, state):
         self.amount = amount
         self.state = state
+        self.tax = float(self.salestax_dict[self.state]) / 100
+
+
+    def tax_amount(self):
+        #tax = float(self.salestax_dict[self.state]) / 100
+        tax_amount = self.amount*self.tax
+        return tax_amount
 
     def calculator(self):
-        tax = float(self.salestax_dict[self.state])
-        return round(self.amount*(1+tax/100), 2)
+        #tax = float(self.salestax_dict[self.state]) / 100
+        return round(self.amount*(1+self.tax), 2)
