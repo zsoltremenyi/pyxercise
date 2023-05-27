f9662b8651d7959f8cf783b63e15f5677ad06672
Zsolt Remenyi <remenyi1zsolt@gmail.com>
2023-05-27 17:35:18 +0100

    program without switch

--- number_to_month/num_to_month.py
+++ number_to_month/num_to_month.py
@@ -1,1 +1,16 @@
 num = int(input('Please enter the number of the month: (1-12) '))
+
+month_dict = {1: 'January',
+              2: 'February',
+              3: 'March',
+              4: 'April',
+              5: 'May',
+              6: 'June',
+              7: 'July',
+              8: 'August',
+              9: 'September',
+              10: 'October',
+              11: 'November',
+              12: 'December'}
+
+print(month_dict[num])
