294c67f289f035dd8cf0e4a52a3f9c64cafe2ca1
Zsolt Remenyi <remenyi1zsolt@gmail.com>
2023-05-28 00:36:16 +0100

    working version

--- number_to_month/num_to_month.py
+++ number_to_month/num_to_month.py
@@ -1,16 +1,7 @@
+import dictionaries
+
+month = dictionaries.month_dict
 num = int(input('Please enter the number of the month: (1-12) '))
 
-month_dict = {1: 'January',
-              2: 'February',
-              3: 'March',
-              4: 'April',
-              5: 'May',
-              6: 'June',
-              7: 'July',
-              8: 'August',
-              9: 'September',
-              10: 'October',
-              11: 'November',
-              12: 'December'}
 
-print(month_dict[num])
+print(month[num])
