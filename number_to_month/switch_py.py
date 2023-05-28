48f6a320d143757aa435ab851866cad3d52a602a
Zsolt Remenyi <remenyi1zsolt@gmail.com>
2023-05-28 22:44:49 +0100

    first version with switch, not to be used

--- number_to_month/switch_py.py
+++ number_to_month/switch_py.py
@@ -1,30 +1,33 @@
-num = input('Please enter the number of the month: (1-12) ')
+# Not to use, first version as written in exercise
 
 
-match num:
-    case '1':
-        print('January')
-    case '2':
-        print('February')
-    case '3':
-        print('March')
-    case '4':
-        print('April')
-    case '5':
-        print('May')
-    case '6':
-        print('June')
-    case '7':
-        print('July')
-    case '8':
-        print('August')
-    case '9':
-        print('September')
-    case '10':
-        print('October')
-    case '11':
-        print('November')
-    case '12':
-        print('December')
-    case _:
-        print('invalid input')
+# num = input('Please enter the number of the month: (1-12) ')
+#
+#
+# match num:
+#     case '1':
+#         print('January')
+#     case '2':
+#         print('February')
+#     case '3':
+#         print('March')
+#     case '4':
+#         print('April')
+#     case '5':
+#         print('May')
+#     case '6':
+#         print('June')
+#     case '7':
+#         print('July')
+#     case '8':
+#         print('August')
+#     case '9':
+#         print('September')
+#     case '10':
+#         print('October')
+#     case '11':
+#         print('November')
+#     case '12':
+#         print('December')
+#     case _:
+#         print('invalid input')
