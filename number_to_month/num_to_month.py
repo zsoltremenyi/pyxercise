8369752595154528f657aa47dc1b5d0522826310
Zsolt Remenyi <remenyi1zsolt@gmail.com>
2023-05-28 22:42:44 +0100

    working, with error handling, still to add translation for error messages

--- number_to_month/num_to_month.py
+++ number_to_month/num_to_month.py
@@ -1,23 +1,12 @@
-import dictionaries
-from translate import Translator
-
-
-month = dictionaries.month_dict
-language = dictionaries.LANGUAGES
-
-
-key_list = list(language.keys())
-value_list = list(language.values())
+from translated_output import Translation
 
 
 language_prompt = input("What language would you like to use? ")
-chosen_language = key_list[value_list.index(language_prompt)]
 
 
-translator = Translator(to_lang=chosen_language)
-translated_input = translator.translate('Please enter the number of the month: (1-12) ')
-num = input(translated_input + " ")
-translated_output = translator.translate(f'The name of the month is {month[num]}.')
-
-
-print(translated_output)
+try:
+    translation = Translation(language_prompt)
+except ValueError as err:
+    print(str(err) + ", please input valid language.")
+else:
+    print(translation.trans_output(translation.trans_input()))
