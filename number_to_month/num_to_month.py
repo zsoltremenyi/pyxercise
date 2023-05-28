cc857c1a6813579bf92fb2461617bb0ab4c99f81
Zsolt Remenyi <remenyi1zsolt@gmail.com>
2023-05-28 01:18:46 +0100

    working translating, no error handling

--- number_to_month/num_to_month.py
+++ number_to_month/num_to_month.py
@@ -5,6 +5,7 @@ from translate import Translator
 month = dictionaries.month_dict
 language = dictionaries.LANGUAGES
 
+
 key_list = list(language.keys())
 value_list = list(language.values())
 
@@ -12,14 +13,11 @@ value_list = list(language.values())
 language_prompt = input("What language would you like to use? ")
 chosen_language = key_list[value_list.index(language_prompt)]
 
+
 translator = Translator(to_lang=chosen_language)
 translated_input = translator.translate('Please enter the number of the month: (1-12) ')
 num = input(translated_input + " ")
-
 translated_output = translator.translate(f'The name of the month is {month[num]}.')
 
+
 print(translated_output)
-
-
-
-#print(month[num])
