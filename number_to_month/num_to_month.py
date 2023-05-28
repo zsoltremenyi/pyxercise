45a14a53e62b462badc70df0a5125668169823e7
Zsolt Remenyi <remenyi1zsolt@gmail.com>
2023-05-28 23:38:19 +0100

    try-except-else block added. final

--- number_to_month/num_to_month.py
+++ number_to_month/num_to_month.py
@@ -7,6 +7,6 @@ language_prompt = input("What language would you like to use? ")
 try:
     translation = Translation(language_prompt)
 except ValueError as err:
-    print(str(err) + ", please input valid language.")
+    raise ValueError(str(err) + ", please input valid language.")
 else:
     print(translation.trans_output(translation.trans_input()))
