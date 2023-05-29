b5f309ab8ba40904d52fcc37ec12efe9e940e501
Zsolt Remenyi <remenyi1zsolt@gmail.com>
2023-05-29 14:11:29 +0100

    simple version

--- comparing_number/comparing.py
+++ comparing_number/comparing.py
@@ -1,1 +1,15 @@
+first = int(input("Enter the first number: "))
+second = int(input("Enter the second number: "))
+third = int(input("Enter the third number: "))
+
+num_list = [first, second, third]
+largest_num = 0
+
+for i in num_list:
+    if not largest_num:
+        largest_list = i
+    elif largest_num < int(i):
+        largest_num = int(i)
+
+print(largest_num)
 
