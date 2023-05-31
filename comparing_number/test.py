c9630ac63fa40e4fa7575bc766a8ff1db43a7f71
Zsolt Remenyi <remenyi1zsolt@gmail.com>
2023-05-31 11:12:15 +0100

    two tests

--- comparing_number/test.py
+++ comparing_number/test.py
@@ -10,6 +10,14 @@ class UnitTests(unittest.TestCase):
         expected_output = 10
         self.assertEqual(actual_output,expected_output)
 
+    def testnegative(self):
+        neg_list = [-1, -4, -19]
+        actual_output = largest_num(neg_list)
+        expected_output = -1
+        self.assertEqual(actual_output, expected_output)
+
+
+
 
 if __name__ == '__main__':
     unittest.main()
