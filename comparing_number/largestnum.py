d60635c4b2103ef74940ff78c3835cdbb2b470cb
Zsolt Remenyi <remenyi1zsolt@gmail.com>
2023-05-31 11:11:48 +0100

    corrected variables, final

--- comparing_number/largestnum.py
+++ comparing_number/largestnum.py
@@ -1,8 +1,10 @@
 def largest_num(numbers):
     max_num = 0
     for i in numbers:
-        if not largest_num:
-            largest_list = i
+        if not max_num:
+            max_num = i
         elif max_num < i:
             max_num = i
     return max_num
+
+print(largest_num([-1, g, -19]))
