0ce15c4a76ee7c1a410a7fb55e6bb314155d67f9
Zsolt Remenyi <remenyi1zsolt@gmail.com>
2023-05-31 01:15:44 +0100

    first test working

--- comparing_number/test.py
+++ comparing_number/test.py
@@ -1,1 +1,15 @@
 import unittest
+from largestnum import largest_num
+
+
+class UnitTests(unittest.TestCase):
+
+    def test1(self):
+        alist = [2, 4, 10]
+        actual_output = largest_num(alist)
+        expected_output = 10
+        self.assertEqual(actual_output,expected_output)
+
+
+if __name__ == '__main__':
+    unittest.main()
