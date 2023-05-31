51ea8e4d673ff860f9faafea772aaf51b6e142de
Zsolt Remenyi <remenyi1zsolt@gmail.com>
2023-05-31 11:32:28 +0100

    with docstring

--- comparing_number/largestnum.py
+++ comparing_number/largestnum.py
@@ -1,10 +1,11 @@
 def largest_num(numbers):
+    """returns the largest number in list"""
+
     max_num = 0
+
     for i in numbers:
         if not max_num:
             max_num = i
         elif max_num < i:
             max_num = i
     return max_num
-
-print(largest_num([-1, g, -19]))
