71065390ada7486f57de0c6c0a402ed74e3da202
Zsolt Remenyi <remenyi1zsolt@gmail.com>
2023-06-01 22:19:03 +0100

    main

--- anagram/anagram.py
+++ anagram/anagram.py
@@ -1,1 +1,11 @@
+from compare import comparing
 
+
+print("Enter two strings and I'll tell you if they are anagrams: ")
+
+
+word1 = input("Enter the first string: ")
+word2 = input("Enter the second string: ")
+
+
+comparing(word1, word2)
