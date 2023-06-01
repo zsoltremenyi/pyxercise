c0d47dbd11e5d14d51fda87d23ea1c4f0c1ae2a6
Zsolt Remenyi <remenyi1zsolt@gmail.com>
2023-06-01 13:03:22 +0100

    working tests (pos,neg,zero values)

--- comparing_number/test.py
+++ comparing_number/test.py
@@ -6,17 +6,21 @@ class UnitTests(unittest.TestCase):
 
     def test1(self):
         alist = [2, 4, 10]
-        actual_output = largest_num(alist)
-        expected_output = 10
-        self.assertEqual(actual_output,expected_output)
+        actual_output1 = largest_num(alist)
+        expected_output = "\nThe largest number in list is: 10"
+        self.assertEqual(actual_output1, expected_output)
 
     def testnegative(self):
         neg_list = [-1, -4, -19]
-        actual_output = largest_num(neg_list)
-        expected_output = -1
-        self.assertEqual(actual_output, expected_output)
+        actual_output2 = largest_num(neg_list)
+        expected_output = "\nThe largest number in list is: -1"
+        self.assertEqual(actual_output2, expected_output)
 
-
+    def test_for_zero(self):
+        list_with_zero = [-4, -2, 0]
+        actual_output3 = largest_num(list_with_zero)
+        expected_output = "\nThe largest number in list is: 0"
+        self.assertEqual(actual_output3,expected_output)
 
 
 if __name__ == '__main__':
