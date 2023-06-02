ed25e13716a71f703b57d17f2ccd5b28d9177f50
Zsolt Remenyi <remenyi1zsolt@gmail.com>
2023-06-02 15:53:32 +0100

    prompt module

---
+++ credit_card/prompts.py
@@ -0,0 +1,14 @@
+def balance():
+    return float(input("What is your balance? "))
+
+
+def card_apr():
+    return float(input("What is the APR on the card (as a percent)? "))
+
+
+def monthly_payment():
+    return float(input("What is the monthly payment you can make? "))
+
+
+def month():
+    return int(input("How many month do you want to pay your credit card in? "))
