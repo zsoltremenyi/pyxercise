3936bcb67446a65c4013f5aecb4baa2dbab5c3f1
Zsolt Remenyi <remenyi1zsolt@gmail.com>
2023-06-02 15:54:56 +0100

    calculations module

--- credit_card/calculator.py
+++ credit_card/calculator.py
@@ -3,9 +3,17 @@ import math
 
 
 def calculateMonthsUntilPaidOff(apr, balance, mpayment):
-    apr = apr/100/365
+    apr = apr / 100 / 365
     balance = round(balance, 2)
     mpayment = round(mpayment, 2)
     n = math.ceil(-1 / 30 * (np.log10(1 + balance / mpayment * (1 - (1 + apr) ** 30)) / np.log10(1 + apr)))
-    print(n)
-    return "It will take %g months to pay off this card." % n
+
+    return "It will take %g month(s) to pay off this card." % n
+
+
+def calculatePaymentForDesireMonth(apr, balance, month):
+    apr = apr / 100 / 365
+    balance = round(balance, 2)
+    mpayment = math.ceil(balance * (1 - (1 + apr) ** 30) / (10 ** ((month * 30 * np.log10(1 + apr)) / -1) - 1))
+
+    return "You have to pay $%g to pay off this card in %g month(s)" % (mpayment, month)
