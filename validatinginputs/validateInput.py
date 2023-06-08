a572dafbc2b5efc4d9f68754f989247aa9f902a7
Zsolt Remenyi <remenyi1zsolt@gmail.com>
2023-06-08 17:58:53 +0200

    validate inputs, if no problems, returns false

---
+++ validatinginputs/validateInput.py
@@ -0,0 +1,18 @@
+from employee import Employee
+import prompting as prm
+
+
+def validate_input():
+    emp = Employee(prm.id(), prm.prompt(), prm.fname(), prm.lname())
+
+    if not emp.employee_id():
+        print()
+    elif not emp.last_name():
+        print()
+    elif not emp.first_name():
+        print()
+    elif not emp.zipcode():
+        print()
+    elif emp.employee_id() and emp.last_name() and emp.first_name() and emp.zipcode():
+        print("There were no errors found.")
+        return False
