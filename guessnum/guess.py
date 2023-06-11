dbef130d3bb1e6d17443d374bb72e8c84e882a71
Zsolt Remenyi <remenyi1zsolt@gmail.com>
2023-06-11 20:01:07 +0200

    first version

--- guessnum/guess.py
+++ guessnum/guess.py
@@ -1,1 +1,23 @@
+from random import randint
+from guessing_module import guessing
+
+
+while True:
+    difficulty = input("Pick a difficulty level (1, 2, or 3): ")
+
+    hard = randint(1, 1000)
+    if difficulty == "1":
+        easy = randint(1, 10)
+        prompt = input("I have my number. What's your guess? ")
+        guessing(easy, prompt)
+    elif difficulty == "2":
+        medium = randint(1, 100)
+        prompt = input("I have my number. What's your guess? ")
+        guessing(medium, prompt)
+    elif difficulty == "3":
+        hard = randint(1, 1000)
+        prompt = input("I have my number. What's your guess? ")
+        guessing(hard,prompt)
+    else:
+        print(f"{difficulty} is not a valid difficulty level. Please try again!")
 
---
+++ guessnum/guessing_module.py
@@ -0,0 +1,13 @@
+def guessing(difficulty, prompt):
+    counter = 0
+    while True:
+        if difficulty == int(prompt):
+            counter += 1
+            print(f"You got it in {counter} guesses!")
+            break
+        elif difficulty > int(prompt):
+            counter += 1
+            prompt = input("Too low guess again: ")
+        elif difficulty < int(prompt):
+            counter += 1
+            prompt = input("Too high guess again: ")
