82d7b25769494bb4bcc1c4620c4dc05f2d777e21
Zsolt Remenyi <remenyi1zsolt@gmail.com>
2023-06-11 20:14:50 +0200

    counts strings as wrong answers as well

--- guessnum/guessing_module.py
+++ guessnum/guessing_module.py
@@ -1,13 +1,13 @@
 def guessing(difficulty, prompt):
     counter = 0
     while True:
-        if difficulty == int(prompt):
+        if str(difficulty) == str(prompt):
             counter += 1
             print(f"You got it in {counter} guesses!")
             return
-        elif difficulty > int(prompt):
+        elif str(difficulty) > str(prompt):
             counter += 1
             prompt = input("Too low guess again: ")
-        elif difficulty < int(prompt):
+        elif str(difficulty) < str(prompt):
             counter += 1
             prompt = input("Too high guess again: ")
