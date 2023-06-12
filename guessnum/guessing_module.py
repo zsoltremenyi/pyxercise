ce883169c6cebb1ef28ebe77ac1040d6a4ddf3b4
Zsolt Remenyi <remenyi1zsolt@gmail.com>
2023-06-12 16:42:31 +0200

    warns if number was already guessed

--- guessnum/guessing_module.py
+++ guessnum/guessing_module.py
@@ -1,13 +1,26 @@
 def guessing(difficulty, prompt):
     counter = 0
+    num_list = []
     while True:
+
         if str(difficulty) == str(prompt):
             counter += 1
             print(f"You got it in {counter} guesses!")
             return
+
         elif str(difficulty) > str(prompt):
+            if prompt in num_list:
+                print(f"Already guessed {prompt}.")
+            elif prompt not in num_list:
+                num_list.append(prompt)
             counter += 1
             prompt = input("Too low guess again: ")
+
         elif str(difficulty) < str(prompt):
+            if prompt in num_list:
+                print(f"Already guessed {prompt}.")
+            elif prompt not in num_list:
+                num_list.append(prompt)
             counter += 1
             prompt = input("Too high guess again: ")
+
