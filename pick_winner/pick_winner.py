c75a43c739e2e77e79c94d983093210f4981f8e2
Zsolt Remenyi <remenyi1zsolt@gmail.com>
2023-06-16 17:54:23 +0200

    main, finding winner and removing them from list

--- pick_winner/pick_winner.py
+++ pick_winner/pick_winner.py
@@ -1,1 +1,19 @@
+from random import choice
+from create_list import creation
 
+
+full_list = []
+while True:
+
+    list_of_contestants = creation()
+    full_list.extend(list(filter(lambda x: x if x else None, list_of_contestants)))
+    winner = choice(full_list)
+
+    print(f"The winner is... {winner}")
+    full_list.remove(winner)
+
+    print(full_list)
+
+    carry_on = input("Would you like to continue (y, n)? ")
+    if carry_on == "n":
+        break
