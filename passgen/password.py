668f23dcc2f20d48de4f7bdf4671078b14373aa1
Zsolt Remenyi <remenyi1zsolt@gmail.com>
2023-06-17 15:18:31 +0200

    lists for characters created.

--- passgen/password.py
+++ passgen/password.py
@@ -1,1 +1,10 @@
+import string
+letter_list = list(string.ascii_letters)
+spec_list = list(string.punctuation)
+num_list = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 
+print(spec_list)
+
+min_length = int(input("What's the minimum length? "))
+spec_ch = int(input("How many special characters? "))
+nums = int(input("How many numbers? "))
